84a712158bd2503bc2f80e6ea1d54906277d996a
instalator <vvvalt@mail.ru>
2017-08-24 23:46:11 +0700

    initial

--- admin/words.js
+++ admin/words.js
@@ -1,8 +1,10 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"template adapter settings": {"de": "Beispiel", "ru": "Пример"},
-	"test1":                    {"en": "Test 1", "de": "Test 1", "ru": "Тест 1"},
-	"test2":                    {"en": "Test 2", "de": "Test 2", "ru": "Тест 2"},
+	"Haier adapter settings": {"de": "Haier adapter settings", "ru": "Haier adapter settings"},
+	"host":                   	{"en": "host", "de": "host", "ru": "host"},
+	"port":                   	{"en": "port", "de": "port", "ru": "port"},
+	"login":                   	{"en": "login", "de": "login", "ru": "login"},
+	"password":                	{"en": "password", "de": "password", "ru": "password"},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
 		"ru": "При сохранении настроек адаптера он сразу же перезапускается"
--- main.js
+++ main.js
@@ -1,45 +1,8 @@
-/**
- *
- * haier adapter
- *
- *
- *  file io-package.json comments:
- *
- *  {
- *      "common": {
- *          "name":         "haier",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js haier Adapter",  // Adapter title shown in User Interfaces
- *          "authors":  [                               // Array of authord
- *              "name <mail@haier.com>"
- *          ]
- *          "desc":         "haier adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
- *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
- *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
- *          "loglevel":     "info"                      // Adapters Log Level
- *      },
- *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
- *          "test1": true,
- *          "test2": 42
- *      }
- *  }
- *
- */
-
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
 "use strict";
-
-// you have to require the utils module and call adapter function
 var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
-
-// you have to call the adapter function and pass a options object
-// name has to be set and has to be equal to adapters folder name and main file name excluding extension
-// adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.haier.0
 var adapter = utils.adapter('haier');
 
-// is called when adapter shuts down - callback has to be called under any circumstances!
+
 adapter.on('unload', function (callback) {
     try {
         adapter.log.info('cleaned everything up...');
@@ -49,7 +12,6 @@ adapter.on('unload', function (callback) {
     }
 });
 
-// is called if a subscribed object changes
 adapter.on('objectChange', function (id, obj) {
     // Warning, obj can be null if it was deleted
     adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
@@ -79,8 +41,6 @@ adapter.on('message', function (obj) {
     }
 });
 
-// is called when databases are connected and adapter received configuration.
-// start here!
 adapter.on('ready', function () {
     main();
 });
